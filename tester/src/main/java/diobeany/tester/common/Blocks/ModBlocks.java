97d30d0666940341d59ad7c0988faecedfe22934
ImTryingMyBestFR <131595988+ImTryingMyBestFR@users.noreply.github.com>
2023-09-19 21:23:28 +0300

    Delete tester directory

--- tester/src/main/java/diobeany/tester/common/Blocks/ModBlocks.java
+++
@@ -1,9 +0,0 @@
-package diobeany.tester.common.Blocks;
-
-import net.minecraftforge.fml.common.registry.GameRegistry;
-
-public class ModBlocks {
-
-    @GameRegistry.ObjectHolder("tester:painted_wood_purple_blinds")
-    public static BlockBaseWood PURPLE_BLINDS;
-}
--- tester/src/main/java/diobeany/tester/proxy/ServerProxy.java
+++
@@ -1,5 +0,0 @@
-package diobeany.tester.proxy;
-
-public class ServerProxy extends CommonProxy {
-
-}
